7b7e68ab87e8ceca0526fce3a1abfe635b49df52
gixi <104705119+gioliotta@users.noreply.github.com>
2023-11-06 23:39:33 -0800

    New link

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -3,10 +3,10 @@ import donwloadIcon from "../assets/download-icon.svg";
 function Download() {
   function pomoLymAPK() {
     const pomolymApkUrl =
-        "https://expo.dev/artifacts/eas/fpF5TWRhfgcnYrgzpRjsMY.apk",
+        "https://expo.dev/artifacts/eas/fPsGgEwHKyfRUNd3yogMFd.apk",
       link = document.createElement("a");
     link.href = pomolymApkUrl;
-    link.download = "pomolym.apk";
+    link.setAttribute("download", "pomolym.apk");
     link.role = "link";
     link.ariaLabel = "Download APK";
     link.click();
@@ -16,7 +16,7 @@ function Download() {
     <div className="container-download">
       <p className="download-text">
         <span onClick={pomoLymAPK}>Download for Android</span>
-        Version: 1.0.0 &nbsp;&nbsp;&nbsp;Size: 30.3 MB
+        Version: 1.0.0 &nbsp;&nbsp;&nbsp;Size: 30,39 MB
       </p>
       <img
         style={{ cursor: "pointer" }}
