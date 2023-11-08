18f7eefe032685294b67acb19bcd5a4caeabaabd
gixi <104705119+gioliotta@users.noreply.github.com>
2023-11-07 16:25:55 -0800

    change version

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -16,7 +16,7 @@ function Download() {
     <div className="container-download">
       <p className="download-text">
         <span onClick={pomoLymAPK}>Download for Android</span>
-        Version: 1.0.0 &nbsp;&nbsp;&nbsp;Size: 30,39 MB
+        Version: 1.0.1 &nbsp;&nbsp;&nbsp;Size: 30,39 MB
       </p>
       <img
         style={{ cursor: "pointer" }}
